11bcf4a0e80e4f28ba713b171b8243f3059c4b33
thasarito <thasarit.pysr@gmail.com>
2021-01-09 09:27:28 +0700

    animate path on render

--- part3/src/components/trainLine.js
+++ part3/src/components/trainLine.js
@@ -1,5 +1,13 @@
+import { useLayoutEffect, useRef, useState } from "react";
+
 export default function TrainLine(props) {
   const { feature, path, mapYear } = props;
+
+  const ref = useRef(null);
+  const [length, setLength] = useState(0);
+  useLayoutEffect(() => {
+    setLength(ref.current.getTotalLength());
+  }, [ref]);
 
   const { stroke, end } = feature.properties;
 
@@ -9,12 +17,29 @@ export default function TrainLine(props) {
     <g className="line">
       {mapYear >= end && (
         <path
+          ref={ref}
+          class={"animate"}
           d={definition}
           fill="transparent"
           stroke={stroke}
           strokeWidth={3}
+          strokeDasharray={length}
+          strokeDashoffset={length}
         />
       )}
+      <style jsx>{`
+        .animate {
+          animation-name: dash;
+          animation-duration: 1.5s;
+          animation-timing-function: linear;
+          animation-fill-mode: forwards;
+        }
+        @keyframes dash {
+          to {
+            stroke-dashoffset: 0;
+          }
+        }
+      `}</style>
     </g>
   );
 }
